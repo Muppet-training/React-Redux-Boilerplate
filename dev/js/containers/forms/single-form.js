9e203ba2399fee840ac8bdf671c3e3d0eaabe6e9
Fed-up <tomcurphey12@gmail.com>
2018-04-10 17:15:06 +1000

    Initial Values - need to add values from database

--- dev/js/containers/forms/single-form.js
+++ dev/js/containers/forms/single-form.js
@@ -7,7 +7,12 @@ let SingleForm = props => {
         <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="firstName">First Name</label>
-                <Field name="firstName" component="input" type="text" />
+                <Field
+                    name="firstName"
+                    component="input"
+                    value="tom"
+                    type="text"
+                />
             </div>
 
             <button type="submit">Submit</button>
@@ -15,9 +20,29 @@ let SingleForm = props => {
     );
 };
 
+const InputText = ({ input, label, meta: { touched, error } }) => (
+    <div>
+        <label htmlFor={input.name}>{label}</label>
+        <input {...input} type="text" />
+        {touched && error && <span className="error">{error}</span>}
+    </div>
+);
+
 SingleForm = reduxForm({
     // a unique name for the form
-    form: 'contact'
+    form: 'contact',
+    enableReinitialize: true, // this is needed!!
+    initialValues: {
+        firstName: 'Tom'
+    }
 })(SingleForm);
+
+// // You have to connect() to any reducers that you wish to connect to yourself
+// InitializeFromStateForm = connect(
+//   state => ({
+//     initialValues: state.account.data // pull initial values from account reducer
+//   }),
+//   { load: loadAccount } // bind account loading action creator
+// )(InitializeFromStateForm)
 
 export default SingleForm;
